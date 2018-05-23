0f1d0babda18196585d0b4a3c62fa0d4f8454c99
Rijn <bxbian951122@gmail.com>
2018-05-22 19:55:09 -0700

    Add new prop showCategoryTitleInSearchResults

--- src/EmojiInput.js
+++ src/EmojiInput.js
@@ -279,6 +279,10 @@ class EmojiInput extends PureComponent {
             .flatten(tempEmoji)
             .value();
 
+        if (!this.props.showCategoryTitleInSearchResults && this.state.searchQuery) {
+            this.emoji = _.filter(this.emoji, c => !c.categoryMarker);
+        }
+
         this.setState({
             dataProvider: dataProvider.cloneWithRows(this.emoji),
         });
@@ -436,6 +440,7 @@ EmojiInput.defaultProps = {
     numColumns: 6,
 
     showCategoryTab: true,
+    showCategoryTitleInSearchResults: false,
     categoryUnhighlightedColor: 'lightgray',
     categoryHighlightColor: 'black',
     enableSearch: true,
@@ -450,7 +455,6 @@ EmojiInput.defaultProps = {
     },
     emojiFontSize: 40,
     categoryFontSize: 20,
-    showCategoryTab: true,
     enableSearch: true,
     resetSearch: false,
 };
@@ -463,6 +467,7 @@ EmojiInput.propTypes = {
     onEmojiSelected: PropTypes.func.isRequired,
 
     showCategoryTab: PropTypes.bool,
+    showCategoryTitleInSearchResults: PropTypes.bool,
     categoryFontSize: PropTypes.number,
     categoryUnhighlightedColor: PropTypes.string,
     categoryHighlightColor: PropTypes.string,
