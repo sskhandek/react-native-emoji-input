f7332a8924ccb63a4ad6b9993844f08a75cb4bd0
Sujay Khandekar <sskhandek@gmail.com>
2018-07-10 10:16:21 -0700

    Adapting for Android

--- example/src/EmojiInput.js
+++ example/src/EmojiInput.js
@@ -78,6 +78,72 @@ const ViewTypes = {
     CATEGORY: 1
 };
 
+// fromCodePoint polyfill
+if (!String.fromCodePoint) {
+    (function() {
+        var defineProperty = (function() {
+            // IE 8 only supports `Object.defineProperty` on DOM elements
+            try {
+                var object = {};
+                var $defineProperty = Object.defineProperty;
+                var result =
+                    $defineProperty(object, object, object) && $defineProperty;
+            } catch (error) {}
+            return result;
+        })();
+        var stringFromCharCode = String.fromCharCode;
+        var floor = Math.floor;
+        var fromCodePoint = function() {
+            var MAX_SIZE = 0x4000;
+            var codeUnits = [];
+            var highSurrogate;
+            var lowSurrogate;
+            var index = -1;
+            var length = arguments.length;
+            if (!length) {
+                return '';
+            }
+            var result = '';
+            while (++index < length) {
+                var codePoint = Number(arguments[index]);
+                if (
+                    !isFinite(codePoint) || // `NaN`, `+Infinity`, or `-Infinity`
+                    codePoint < 0 || // not a valid Unicode code point
+                    codePoint > 0x10ffff || // not a valid Unicode code point
+                    floor(codePoint) != codePoint // not an integer
+                ) {
+                    throw RangeError('Invalid code point: ' + codePoint);
+                }
+                if (codePoint <= 0xffff) {
+                    // BMP code point
+                    codeUnits.push(codePoint);
+                } else {
+                    // Astral code point; split in surrogate halves
+                    // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                    codePoint -= 0x10000;
+                    highSurrogate = (codePoint >> 10) + 0xd800;
+                    lowSurrogate = (codePoint % 0x400) + 0xdc00;
+                    codeUnits.push(highSurrogate, lowSurrogate);
+                }
+                if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+                    result += stringFromCharCode.apply(null, codeUnits);
+                    codeUnits.length = 0;
+                }
+            }
+            return result;
+        };
+        if (defineProperty) {
+            defineProperty(String, 'fromCodePoint', {
+                value: fromCodePoint,
+                configurable: true,
+                writable: true
+            });
+        } else {
+            String.fromCodePoint = fromCodePoint;
+        }
+    })();
+}
+
 class EmojiInput extends React.PureComponent {
     constructor(props) {
         super(props);
