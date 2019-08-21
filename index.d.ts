0ab2790c1cdd57306cfa5ae0158a0b6c6cc3e9a5
aminmahrami <amin.mahrami@yahoo.com>
2019-08-21 14:16:03 +0200

    Add typing

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { EmojiData } from "emoji-datasource";
+import React from "react";
+import { StyleProp, TextStyle } from "react-native";
+
+interface Emoji {
+    category: string;
+    char: string;
+    keywords: string[];
+    lib: EmojiData;
+}
+
+// NOTE: props are not complete
+interface Props {
+    onEmojiSelected: (emoji: Emoji) => void;
+    keyboardBackgroundColor?: string;
+    categoryLabelTextStyle?: StyleProp<TextStyle>;
+    categoryLabelHeight?: number;
+    numColumns?: number;
+    emojiFontSize?: number;
+    onCategoryPress?: (category: string) => void;
+    enableSearch?: boolean;
+}
+
+export default class EmojiInput extends React.Component<Props> {}
